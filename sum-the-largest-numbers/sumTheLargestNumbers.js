21a4f3f62f1912a31fd9ce3205b19e2c74db2ee6
Ted Ogmundson <togmund@gmail.com>
2019-10-03 20:15:33 +0000

    final commit sumLargestNumbers, solution confirmed with tests

--- sum-the-largest-numbers/sumTheLargestNumbers.js
+++ sum-the-largest-numbers/sumTheLargestNumbers.js
@@ -1,27 +1,29 @@
 /*
-11:15
 Purpose of the sumLargestNumbers function:
-  In this exercise, we will be given an array of 2 or more numbers. We will then have to find the two largest numbers in that array, and sum them together.
+  Find the two largest numbers in a given array and sum them together
 Parameters:
-  Array | Array to be searched
-Conditions:
-  If the Value never occurs, return -1
+  Array | Data to be searched
 Output:
   Number | Sum of the two largest values
 */
 
-let sumLargestNumbers = function(data) { // Hand us an array
-  let sum = []; // Empty array for filling
-  sum = data.sort(); // Sort the contents of the incoming array and dump it into our sum array
-  // console.log(sum);
-  sum.splice(2,sum.length); // Reduce the sum array to the first two indexes
-  // console.log(sum);
-  sum.length === 2 // Validate that the array we have created and sliced has exactly the number of values to sum
-    ? sum = sum[0] + sum[1]
-    : sum = "Gimmie an array with at least two numbers, you doofus!"
-  return sum;
+const sumLargestNumbers = function (data) { // Hand us an array
+  // console.log(data); // Log the argument for debugging
+  let sum = []; // Create an empty array for filling
+  sum = data.sort(function descendingSort(a, b) { // Sort the contents of the argument and dump it into our sum array
+    // Pass the desendingSort function as a parameter for the sort method
+    return b - a; // Reversed the logic of the ascending number sort here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+  });
+  // console.log(sum); // Log the sorted array for debugging
+  // sum.splice(2,sum.length); // Reduce the sum array to the first two indexes // Deprecated after deciding to re-assign sum as the final value
+  // console.log(sum); // Log the spliced array for debugging
+  sum.length > 1 // Validate that the array we have created and sliced has exactly the number of values to sum
+    ? sum = sum[0] + sum[1] // Set the sum variable to be the totalled value of the first and second index of the constructed sum array
+    : sum = "Gimmie an array with at least two numbers, you doofus!" // Error message in case the sum array has only one value
+  return sum; // Return the result of the function
 };
 
+// Tests
 console.log(sumLargestNumbers([1, 10]));
 console.log(sumLargestNumbers([1, 2, 3]));
 console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
