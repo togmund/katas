c98065492471255965e0ea0b6d23a5e8a7188483
Ted Ogmundson <togmund@gmail.com>
2019-10-03 21:54:17 +0000

    latest version of conditional sum. doesn't work with odds. moving my ternary test inot my values switch.

--- conditional-sums/conditionalSums.js
+++ conditional-sums/conditionalSums.js
@@ -8,13 +8,34 @@ Output:
   Number | Sum of all values in the array that match the condition.
 */
 
-const conditionalSum = function(values, condition) {
-  let sum = values;
-  let conditions = condition;
-  return sum + conditions;
+const conditionalSum = function(values, condition) { // console.log(values + condition);
+  let sum = 0;
+  let test;
+  condition === "even"
+    ? test = true
+    : condition === "odd"
+      ? test = false
+      : sum = "Invalid Condition. Use 'even' or 'odd'."; // console.log(test);
+  values.forEach(function(value){ // console.log(value);
+    switch (test) {
+      case test === undefined:
+        break;
+      case test && value % 2 === 0:
+        sum += value; // console.log("even add:" + value + "\n" + "running sum:" + sum);
+        break;
+      case test === false && value % 2 !== 0:
+        sum += value; // console.log("odd add:" + value + "\n" + "running sum:" + sum);
+        break;
+    }
+  })
+  return "Final Sum: " + sum;
 };
+
 // Tests
 console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log("\n");
 console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log("\n");
 console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log("\n");
 console.log(conditionalSum([], "odd"));
