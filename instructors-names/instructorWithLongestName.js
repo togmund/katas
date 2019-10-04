8b34bf9f9e531ff81139822e76de945f7ed5f7db
Ted Ogmundson <togmund@gmail.com>
2019-10-04 22:30:32 +0000

    final update to instructorWithLongestName.js

--- instructors-names/instructorWithLongestName.js
+++ instructors-names/instructorWithLongestName.js
@@ -9,20 +9,27 @@ Output:
   String | The longest name
 */
 
-const instructorWithLongestName = function(instructors) {
-  let output;
-  output = instructors;
-  return output;
+const instructorWithLongestName = function (instructors) {
+  // console.log("Input: ", instructors) // Initial input for debugging
+  let longest = []; // Empty array to house the sorted list, then the eventual output of our function
+  longest = instructors.sort(function descendingSort(a, b) { // Borrowed the sort algorithm from sumTheLargestNumbers.js
+    return b.name.length - a.name.length; // This time I'm sorting based on the length of just the names in the objects in the array, longest to shortest
+  });
+  longest.length > 0 // Let's make sure we have at least one object ot display
+    ? longest = longest[0] // The first object in the array should be the one with the longest name value. Set our longest output to just that value
+    : longest = "Gimmie an array with at least one object, you doofus"; // Error message in case the sum array has no values
+  return longest;
 };
 
 console.log(instructorWithLongestName([
-  {name: "Samuel", course: "iOS"},
-  {name: "Jeremiah", course: "Web"},
-  {name: "Ophilia", course: "Web"},
-  {name: "Donald", course: "Web"}
+  { name: "Samuel", course: "iOS" },
+  { name: "Jeremiah", course: "Web" },
+  { name: "Ophilia", course: "Web" },
+  { name: "Donald", course: "Web" }
 ]));
+console.log("\n");
 console.log(instructorWithLongestName([
-  {name: "Matthew", course: "Web"},
-  {name: "David", course: "iOS"},
-  {name: "Domascus", course: "Web"}
+  { name: "Matthew", course: "Web" },
+  { name: "David", course: "iOS" },
+  { name: "Domascus", course: "Web" }
 ]));
