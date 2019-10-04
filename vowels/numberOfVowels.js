fb76d226f24f7ac7e13085f19663d14a6a5e174b
Ted Ogmundson <togmund@gmail.com>
2019-10-04 20:41:58 +0000

    final commit. easy peasy

--- vowels/numberOfVowels.js
+++ vowels/numberOfVowels.js
@@ -9,15 +9,30 @@ Output:
   Number | sum of all vowels in the input string
 */
 
-const numberOfVowels = function (inputString) {
-  console.log(inputString);
-  console.log(inputString.length);
-  let sum = 0;
-  const vowels = ["a", "e", "i", "o", "u"]
-  for (i = 0; i < inputString.length; i++) {
-    sum++;
+const numberOfVowels = function (inputString) { // Take in your string
+  // console.log(inputString); // Log initial input for debugging
+  // console.log(inputString.length); // Log initial input lenght for debugging
+  let sum = 0; // Assume no vowels to begin
+  for (i = 0; i < inputString.length; i++) { // Loop through the string
+    switch (inputString[i]) { // Compare string indexes based on the loop
+      case "a": 
+        sum++; // Iterate sum up when you find a vowel // console.log("plus one for a, current count: " + sum); // Log each iteration and the updated sum for debugging
+        break;
+      case "e":
+        sum++; // console.log("plus one for e, current count: " + sum);
+        break;
+      case "i":
+        sum++; // console.log("plus one for i, current count: " + sum);
+        break;
+      case "o":
+        sum++; // console.log("plus one for o, current count: " + sum);
+        break;
+      case "u":
+        sum++; // console.log("plus one for u, current count: " + sum);
+        break;
+    }
   }
-  return sum;
+  return sum; // Return final count
 };
 
 console.log(numberOfVowels("orange") + "\n");
